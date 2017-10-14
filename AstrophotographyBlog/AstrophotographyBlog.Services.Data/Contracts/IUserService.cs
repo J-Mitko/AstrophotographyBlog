d06c151d916ee674e7184e8daa8f0ac4e400df52
Dimitar Pelev <dimitarpelev@gmail.com>
2017-10-14 20:52:40 +0300

    post controller

---
+++ AstrophotographyBlog/AstrophotographyBlog.Services.Data/Contracts/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using AstrophotographyBlog.Data.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstrophotographyBlog.Services.Data.Contracts
+{
+    public interface IUserService : IService
+    {
+        IQueryable<User> GetAll();
+
+        User GetById(Guid id);
+    }
+}
---
+++ AstrophotographyBlog/AstrophotographyBlog.Services.Data/UserService.cs
@@ -0,0 +1,33 @@
+﻿using AstrophotographyBlog.Services.Data.Contracts;
+using System;
+using System.Linq;
+using AstrophotographyBlog.Data.Models.Users;
+using AstrophotographyBlog.Data.Repositories.Contracts;
+using AstrophotographyBlog.Data;
+
+namespace AstrophotographyBlog.Services.Data
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IPostRepository postRepository;
+        private readonly ISaveContext saveContext;
+
+        public UserService(IUserRepository userRepository, IPostRepository postRepository, ISaveContext saveContext)
+        {
+            this.userRepository = userRepository;
+            this.postRepository = postRepository;
+            this.saveContext = saveContext;
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return userRepository.All;
+        }
+
+        public User GetById(Guid id)
+        {
+            return userRepository.Get(id);
+        }
+    }
+}
