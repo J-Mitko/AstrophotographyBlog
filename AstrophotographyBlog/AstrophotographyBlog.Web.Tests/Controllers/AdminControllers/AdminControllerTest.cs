ec9260c019cdc724472d43d61eaa67f5cfb8acd3
Dimitar Pelev <dimitarpelev@gmail.com>
2017-10-15 22:20:14 +0300

    AdminControllerTest

---
+++ AstrophotographyBlog/AstrophotographyBlog.Web.Tests/Controllers/AdminControllers/AdminControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using AstrophotographyBlog.Web.Areas.Admin.Controllers;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace AstrophotographyBlog.Web.Tests.Controllers.AdminControllers
+{
+    [TestFixture]
+    public class AdminControllerTest
+    {
+        [Test]
+        public void IndexActionShould_ReturnViewWithoutName()
+        {
+            // Arrange
+            var controller = new AdminController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(string.Empty, result.ViewName);
+        }
+
+        [Test]
+        public void IndexActionShould_CallViewWithoutModel()
+        {
+            // Arrange
+            var controller = new AdminController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(null, result.Model);
+        }
+
+        [Test]
+        public void IndexActionShould_ReturnNotNullViewResult()
+        {
+            // Arrange
+            var controller = new AdminController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
--- AstrophotographyBlog/AstrophotographyBlog.Web/Areas/Admin/Controllers/NewPostAdminController.cs
+++ AstrophotographyBlog/AstrophotographyBlog.Web/Areas/Admin/Controllers/NewPostAdminController.cs
@@ -1,11 +1,7 @@
-﻿using AstrophotographyBlog.Data.Models;
-using AstrophotographyBlog.Services.Data.Contracts;
+﻿using AstrophotographyBlog.Services.Data.Contracts;
 using AstrophotographyBlog.Web.Areas.Admin.Models;
 using Microsoft.AspNet.Identity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AstrophotographyBlog.Web.Areas.Admin.Controllers
